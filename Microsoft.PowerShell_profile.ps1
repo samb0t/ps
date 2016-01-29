32b14dfb27a39734b299d41252c135390ed6a03e
samb0t <samsnee123@yahoo.com>
2016-01-29 15:57:51 -0600

    ps files

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+﻿
+# Load posh-git example profile
+. '~\AppData\Local\GitHub\PoshGit_869d4c5159797755bc04749db47b166136e59132\profile.example.ps1'
+
+$ProfileRoot = (Split-Path -Parent $MyInvocation.MyCommand.Path)
+$env:path += ";$ProfileRoot"
+
+# turn powershell into visual studio developer console
+Invoke-Environment '"%vs140comntools%\vsvars32.bat"'
+
+# to call something in the msbuild directory '&$msb\csi.exe' for example
+	# no need to cd into msb for csi since we're already a dev console
+$msb = "${env:ProgramFiles(x86)}" + "\MSBuild\14.0\Bin\"
+
